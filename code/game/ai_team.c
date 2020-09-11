870b49718a56edd2befa4fe63a39ec04430744ff
ToKu <toku@gmail.com>
2020-09-11 23:30:03 +0200

    AI: fix 'BotCTFOrders_BothFlagsNotAtBase' (case 3/default). Changed to 'cmd_returnflag' from 'cmd_getflag' because we are in a passive strategy (not really much different).

--- code/game/ai_team.c
+++ code/game/ai_team.c
@@ -3355,7 +3355,7 @@ void BotCTFOrders_BothFlagsNotAtBase(bot_state_t *bs) {
 					}
 
 					ClientName(other, name, sizeof(name));
-					BotAI_BotInitialChat(bs, "cmd_returnflag", name, NULL); // Tobias CHECK: was "cmd_getflag" (but VOICECHAT_RETURNFLAG)!
+					BotAI_BotInitialChat(bs, "cmd_returnflag", name, NULL);
 					BotSayTeamOrder(bs, other);
 					BotSayVoiceTeamOrder(bs, other, VOICECHAT_RETURNFLAG);
 					break;
@@ -3415,9 +3415,9 @@ void BotCTFOrders_BothFlagsNotAtBase(bot_state_t *bs) {
 						}
 
 						ClientName(teammates[numteammates - i - 1], name, sizeof(name));
-						BotAI_BotInitialChat(bs, "cmd_getflag", name, NULL);
+						BotAI_BotInitialChat(bs, "cmd_returnflag", name, NULL);
 						BotSayTeamOrder(bs, teammates[numteammates - i - 1]);
-						BotSayVoiceTeamOrder(bs, teammates[numteammates - i - 1], VOICECHAT_GETFLAG); // Tobias CHECK: was VOICECHAT_RETURNFLAG (but "cmd_getflag")!
+						BotSayVoiceTeamOrder(bs, teammates[numteammates - i - 1], VOICECHAT_RETURNFLAG);
 					}
 
 					break;
@@ -3550,7 +3550,7 @@ void BotCTFOrders_BothFlagsNotAtBase(bot_state_t *bs) {
 						ClientName(teammates[numteammates - i - 1], name, sizeof(name));
 						BotAI_BotInitialChat(bs, "cmd_getflag", name, NULL);
 						BotSayTeamOrder(bs, teammates[numteammates - i - 1]);
-						BotSayVoiceTeamOrder(bs, teammates[numteammates - i - 1], VOICECHAT_GETFLAG); // Tobias CHECK: was VOICECHAT_RETURNFLAG (but "cmd_getflag")!
+						BotSayVoiceTeamOrder(bs, teammates[numteammates - i - 1], VOICECHAT_GETFLAG);
 					}
 
 					break;
