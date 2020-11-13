9de39a704776417ae4a412a787415605f9c9b465
ToKu <toku@gmail.com>
2020-11-13 16:13:36 +0100

    UI: fix broken team orders after (ingame) server start.

--- code/ui/ui_main.c
+++ code/ui/ui_main.c
@@ -4153,6 +4153,8 @@ static void UI_RunMenuScript(char **args) {
 					}
 				}
 			}
+			// Tobias NOTE: update the ui cvars, otherwise bot orders will not work anymore after the new map has loaded ('UI_NotTeamMember_HandleKey')
+			UI_UpdateCvars();
 // Tobias END
 		} else if (Q_stricmp(name, "updateSPMenu") == 0) {
 			UI_SetCapFragLimits(qtrue);
